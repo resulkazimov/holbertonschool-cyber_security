95921ff9f46eba2230a4a30424051a350734dad7
resulkazimov <rsulkazimov29@gmail.com>
2026-08-16 17:31:15 +0000

    Fix task 5 script newline output for checker

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E "new user|useradd" auth.log | grep -o "name=[^,]*" | cut -d'=' -f2 | sort -u | tr '\n' ',' | sed 's/,$//'
+grep -E "new user|useradd" auth.log | grep -o "name=[^,]*" | cut -d'=' -f2 | sort -u | tr '\n' ',' | sed 's/,$//' && echo ""
